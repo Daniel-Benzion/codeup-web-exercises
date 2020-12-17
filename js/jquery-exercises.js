a07d212f449e7ec75a19f17bed634f0d2727366e
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-12-17 17:13:04 -0500

    jQuery selectors

---
+++ js/jquery-exercises.js
@@ -0,0 +1,25 @@
+//"use strict";
+//
+// $(document).ready(function() {
+//     alert("The DOM has finished loading.");
+// })
+$(document).ready(function () {
+    var myHeading = $('h1').html();
+
+    alert(myHeading);
+
+    $('.codeup').css('border', '2px solid red');
+
+    $('li').css('font-size', '20px');
+
+    $('h1,p,li').css('background-color', 'grey');
+
+    //$('#container').css('color','white');
+
+    //$('#container').css('background-color', 'black');
+
+    //$('#container').css( {
+       // 'color': 'white',
+       // 'background-color': 'black'
+   // });
+});
