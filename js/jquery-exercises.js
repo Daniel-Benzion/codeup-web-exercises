b4ddd5efc3b3c23b2b78dde137271bf0b14edd9c
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-12-18 10:29:49 -0500

    jQuery mouse events exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -3,6 +3,7 @@
 // $(document).ready(function() {
 //     alert("The DOM has finished loading.");
 // })
+/*
 $(document).ready(function () {
     var myHeading = $('h1').html();
 
@@ -14,4 +15,26 @@ $(document).ready(function () {
 
     $('h1,p,li').css('background-color', 'grey');
 
-});
+});*/
+
+let h1Color = false;
+$('h1').click(function(e) {
+    if (!h1Color){
+        $(this).css('background', '#ff6f00');
+        h1Color = true;
+    } else {
+        $(this).css('background', '#ffffff');
+        h1Color = false;
+    }
+})
+
+$('p').dblclick(function(e) {
+    $(this).css('font-size', '18px');
+    $(this).text('jQuery Exercises, but slightly larger than before');
+})
+
+$('li').hover(function() {
+    $(this).css('color', '#FF0000');
+}, function() {
+    $(this).css('color', '#000000')
+})
